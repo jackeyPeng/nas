b10bdaa0240a0053f347effff54de2cc9b9b2d6c
jacky <13917478355@163.com>
2026-08-18 09:29:57 +0800

    deploy: 新增 [REDACTED] (fm) 到部署脚本服务器列表

--- scripts/deploy-nas-panel.sh
+++ scripts/deploy-nas-panel.sh
@@ -27,6 +27,8 @@ deploy_to() {
         user="jacky"
     elif [ "$ip" = "[REDACTED]" ]; then
         user="fm"
+    elif [ "$ip" = "[REDACTED]" ]; then
+        user="fm"
     else
         echo "未知服务器: $ip"
         return 1
